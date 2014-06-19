9e09b7b813345798ff8f3f0ba113a38a9f9b0a56
Andrew Proshkin <zx80live@gmail.com>
2014-06-19 23:10:59 +0400

    #

---
+++ src/main/scala/com/zx80live/examples/crazyeights/CardConsoleView.scala
@@ -0,0 +1,42 @@
+package com.zx80live.examples.crazyeights
+
+/**
+ *
+ * @author Andrew Proshkin
+ */
+case class CardConsoleView(card: Card) {
+
+  override def toString: String = {
+    import Rank._
+    import Suit._
+    val suit = card.suit
+    val rank = card.rank
+
+    val s: String = suit match {
+      case Spades => "\u2660"
+      case Hearts => "\u2665"
+      case Diamonds => "\u2666"
+      case Clubs => "\u2663"
+      case Special => ""
+    }
+
+    val r: String = rank match {
+      case Two => " 2"
+      case Three => " 3"
+      case Four => " 4"
+      case Five => " 5"
+      case Six => " 6"
+      case Seven => " 7"
+      case Eight => " 8"
+      case Nine => " 9"
+      case Ten => "10"
+      case Jack => " J"
+      case Queen => " Q"
+      case King => " K"
+      case Ace => " A"
+      case BlackJoker => " \u2605 "
+      case WhiteJoker => " \u2606 "
+    }
+    s"[$r$s]"
+  }
+}
