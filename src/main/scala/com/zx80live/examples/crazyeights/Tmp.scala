36d9208fb362a08c1ceeb7e6ec0023b09bbb5974
Andrew Proshkin <zx80live@gmail.com>
2014-06-20 19:39:58 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/Tmp.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/Tmp.scala
@@ -2,8 +2,6 @@ package com.zx80live.examples.crazyeights
 
 import com.zx80live.examples.crazyeights.model._
 
-import scala.collection.immutable.Iterable
-
 /**
  *
  * @author Andrew Proshkin
@@ -14,26 +12,22 @@ object Tmp extends App with MovePatterns {
   import com.zx80live.examples.crazyeights.model.Suit._
 
   // 2D, 3C, 2C, AD,
-  val cards = Card(Two, Diamonds) :: Card(Three, Clubs) :: Card(Two, Clubs) :: Card(Ace, Diamonds) :: Card(Eight, Diamonds) :: Card(Eight, Spades) :: Card(Two, Spades) :: Card(Ace, Clubs) :: Nil
+  val cards = Card(Two, Diamonds) :: Card(Three, Clubs) :: Card(Two, Clubs) :: Card(Ace, Diamonds) :: Card(Eight, Diamonds) :: Card(Eight, Spades) :: Card(Two, Spades) :: Card(Ace, Clubs) :: Card(WhiteJoker) :: Nil
+  val cards2 = Card(Two, Diamonds) :: Card(Three, Clubs) :: Card(Two, Clubs) :: Card(Ace, Diamonds) :: Card(Eight, Diamonds) :: Card(Ace, Clubs) :: Nil
 
   cards foreach print
   println()
 
 
-  def printPreferredPatternsBySuit(current: Card, cards: List[Card]) = {
-    println(s"\n$current <- current\npreferred by suit:")
-    val preferredBySuit = cards.filter(c => c.suit == current.suit && c.rank != Eight)
-    val byCurrentSuit: (Card, Card) => Boolean = (c1, _) => c1.suit == current.suit
 
 
-    val preferredPatterns = for (g <- cards.groupBy(_.rank).toList.sortBy(-_._2.size); ps <- preferredBySuit; if g._1 == ps.rank) yield g._2 sortWith byCurrentSuit
-
-    preferredPatterns foreach println
-    println()
-  }
-
-  printPreferredPatternsBySuit(Card(Four, Diamonds), cards)
-  printPreferredPatternsBySuit(Card(Four, Clubs), cards)
-  printPreferredPatternsBySuit(Card(Four, Spades), cards)
+  findPreferredBySuit(Card(Four, Diamonds), cards) foreach print
+  println()
+  findPreferredBySuit(Card(Four, Clubs), cards) foreach print
+  println()
+  findPreferredBySuit(Card(Four, Spades), cards) foreach print
+  println()
+  findPreferredBySuit(Card(Four, Spades), cards2) foreach print
+  println()
 
 }
--- src/main/scala/com/zx80live/examples/crazyeights/model/MovePatterns.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/model/MovePatterns.scala
@@ -1,19 +1,51 @@
 package com.zx80live.examples.crazyeights.model
 
-import scala.collection.generic.FilterMonadic
-
 /**
  *
  * @author Andrew Proshkin
  */
 trait MovePatterns {
 
-  import Rank._
-  import Suit._
+  import com.zx80live.examples.crazyeights.model.Rank.Eight
+  import com.zx80live.examples.crazyeights.model.Suit.Special
 
-  def findPreferablePatternBySuit(current: Card, cards: List[Card]): Option[List[Card]] = ???
+  /**
+   * Find preferred move patterns by current suit.
+   * Special cards (e.g. "eights", "jokers" will be filtered)
+   *
+   * @example {{{
+   *
+   *           cards:   [ 2♦][ 3♣][ 2♣][ A♦][ 8♦][ 8♠][ 2♠][ A♣][ ☆ ]
+   *           current: [ 4♦]
+   *
+   *           preferred move patterns:
+   *                (0) [ 2♦], [ 2♣], [ 2♠]   <- highest priority (because there are maximum cards for move)
+   *                (1) [ A♦], [ A♣]
+   *          }}}
+   *
+   * @param current - current card
+   * @param cards - player's cards
+   * @return preferred move patterns sorted by priority
+   */
+  def findPreferredBySuit(current: Card, cards: List[Card]): Option[List[List[Card]]] = {
+    val preferredCardsBySuit = cards.filter(c => c.suit == current.suit && c.rank != Eight && c.suit != Special)
+    val cardsGroupedByRank = cards.groupBy(_.rank).toList.sortBy(-_._2.size)
 
-  def findPreferablePatternByRank(current: Card, cards: List[Card]): Option[List[Card]] = ???
+    val byCurrentSuitSorter: (Card, Card) => Boolean = (c1, _) => c1.suit == current.suit
+
+    val result = for {
+      c <- cardsGroupedByRank
+      pc <- preferredCardsBySuit
+      if c._1 == pc.rank
+    } yield c._2 sortWith byCurrentSuitSorter
+
+    result match {
+      case List() => None
+      case _ => Some(result)
+    }
+  }
+
+  def findPreferableByRank(current: Card, cards: List[Card]): Option[List[Card]] = ???
 
 
 }
