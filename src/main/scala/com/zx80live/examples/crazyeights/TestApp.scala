1a5ccf858c484a0bd4efccff826effdb48af0fe2
Andrew Proshkin <zx80live@gmail.com>
2014-06-21 02:37:17 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/TestApp.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/TestApp.scala
@@ -1,10 +1,7 @@
 package com.zx80live.examples.crazyeights
 
-import com.zx80live.examples.crazyeights.model.Rank._
-import com.zx80live.examples.crazyeights.model.Suit._
 import com.zx80live.examples.crazyeights.model.{Card, Rank, Suit}
 
-import scala.util.matching.Regex
 
 /**
  *
@@ -20,68 +17,10 @@ object TestApp extends App {
     Card(Rank.BlackJoker, Suit.Special) :: Card(Rank.WhiteJoker, Suit.Special) :: deck52
   }
 
-  import scala.language.implicitConversions
+  import com.zx80live.examples.crazyeights.model.ConversionUtils._
 
-  implicit class RegexContext(sc: StringContext) {
-    def r = new Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
-  }
+import scala.language.implicitConversions
 
-  implicit def string2Rank(str: String): Option[Rank.Value] = str.trim match {
-    case "2" => Some(Two)
-    case "3" => Some(Three)
-    case "4" => Some(Four)
-    case "5" => Some(Five)
-    case "6" => Some(Six)
-    case "7" => Some(Seven)
-    case "8" => Some(Eight)
-    case "9" => Some(Nine)
-    case "10" => Some(Ten)
-    case "J" => Some(Jack)
-    case "Q" => Some(Queen)
-    case "K" => Some(King)
-    case "A" => Some(Ace)
-    case "\u2605" | "BJ" => Some(BlackJoker)
-    case "\u2606" | "WJ" => Some(WhiteJoker)
-    case _ => None
-  }
-
-  implicit def string2Suit(str: String): Option[Suit.Value] = str.trim match {
-    case "\u2660" | "S" => Some(Spades)
-    case "\u2665" | "H" => Some(Hearts)
-    case "\u2666" | "D" => Some(Diamonds)
-    case "\u2663" | "C" => Some(Clubs)
-    case _ => None
-  }
-
-  /**
-   * 2♦,
-   * ☆
-   *
-   * @param str - 
-   * @return
-   */
-  implicit def string2Card(str: String): Option[Card] = str match {
-    case r"\s*(\u2605|\u2606|WJ|BJ)$rank\s*" =>
-      (rank: Option[Rank.Value]) match {
-        case Some(rv) => Some(Card(rv))
-        case (None) => None
-      }
-    case r"\s*(\d+|\w)$rank\s*(\w|\u2660|\u2665|\u2666|\u2663)$suit\s*" =>
-      (rank: Option[Rank.Value], suit: Option[Suit.Value]) match {
-        case (None, _) | (_, None) | (None, None) => None
-        case (Some(rv), Some(sv)) => Some(Card(rv, sv))
-      }
-
-    case _ => None
-  }
-
-  implicit def string2CardsList(str: String): Option[List[Card]] = {
-    val xs: List[Option[Card]] = str.split(",").toList.map(c => c: Option[Card])
-    (xs.flatten: List[Card]) match {
-      case (List()) => None
-      case (value) => Some(value)
-    }
-  }
 
   val r: Option[Rank.Value] = "WJ"
   println(r)
@@ -92,7 +31,7 @@ object TestApp extends App {
   println(d)
 
   //println(List[Option[Card]]("2D", "3S", "BJ", "UNKNOWN", "WJ").flatten)
-  println("2D, 3S, BJ, UNKNOWN, WJ": Option[List[Card]])
+  println("2D , 3 ♣, 3S, BJ, UNKNOWN, 2♦  , WJ": Option[List[Card]])
   println("ABCD,A": Option[List[Card]])
 
   //println("2D,3D,4S": Option[List[Card]])
@@ -101,8 +40,6 @@ object TestApp extends App {
   //  val b: Option[Suit.Value] = " D "
   //  println(a + " " + b)
 
-
-  implicit def string2Cards(str: String): List[Card] = ???
 
   lazy val deck54: List[Card] = createDeck54
   //  deck54 foreach println
---
+++ src/main/scala/com/zx80live/examples/crazyeights/model/ConversionUtils.scala
@@ -0,0 +1,97 @@
+package com.zx80live.examples.crazyeights.model
+
+import com.zx80live.examples.crazyeights.model.Rank._
+import com.zx80live.examples.crazyeights.model.Suit._
+import scala.language.implicitConversions
+import scala.util.matching.Regex
+
+/**
+ * Utils for conversion string to game model
+ *
+ * @author Andrew Proshkin
+ */
+object ConversionUtils {
+
+  /**
+   * Allow to use regex into string interpolation
+   *
+   * @param sc -
+   */
+  implicit class RegexContext(sc: StringContext) {
+    def r = new Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+  }
+
+  /**
+   * Converts string to card rank
+   *
+   * @param str - card rank string representation
+   * @return
+   */
+  implicit def string2Rank(str: String): Option[Rank.Value] = str.trim match {
+    case "2" => Some(Two)
+    case "3" => Some(Three)
+    case "4" => Some(Four)
+    case "5" => Some(Five)
+    case "6" => Some(Six)
+    case "7" => Some(Seven)
+    case "8" => Some(Eight)
+    case "9" => Some(Nine)
+    case "10" => Some(Ten)
+    case "J" => Some(Jack)
+    case "Q" => Some(Queen)
+    case "K" => Some(King)
+    case "A" => Some(Ace)
+    case "\u2605" | "BJ" => Some(BlackJoker)
+    case "\u2606" | "WJ" => Some(WhiteJoker)
+    case _ => None
+  }
+
+  /**
+   * Converts string to card suit
+   *
+   * @param str - card suit string representation
+   * @return
+   */
+  implicit def string2Suit(str: String): Option[Suit.Value] = str.trim match {
+    case "\u2660" | "S" => Some(Spades)
+    case "\u2665" | "H" => Some(Hearts)
+    case "\u2666" | "D" => Some(Diamonds)
+    case "\u2663" | "C" => Some(Clubs)
+    case _ => None
+  }
+
+  /**
+   * Converts string to card
+   *
+   * @param str - card string representation
+   * @return
+   */
+  implicit def string2Card(str: String): Option[Card] = str match {
+    case r"\s*(\u2605|\u2606|WJ|BJ)$rank\s*" =>
+      (rank: Option[Rank.Value]) match {
+        case Some(rv) => Some(Card(rv))
+        case (None) => None
+      }
+    case r"\s*(\d+|\w)$rank\s*(\w|\u2660|\u2665|\u2666|\u2663)$suit\s*" =>
+      (rank: Option[Rank.Value], suit: Option[Suit.Value]) match {
+        case (None, _) | (_, None) | (None, None) => None
+        case (Some(rv), Some(sv)) => Some(Card(rv, sv))
+      }
+
+    case _ => None
+  }
+
+  /**
+   * Converts string to cards list
+   *
+   * @param str - cards list string representation delimited by ","
+   * @return
+   */
+  implicit def string2CardsList(str: String): Option[List[Card]] = {
+    val xs: List[Option[Card]] = str.split("\\s*,\\s*").toList.map(c => c: Option[Card])
+    (xs.flatten: List[Card]) match {
+      case (List()) => None
+      case (value) => Some(value)
+    }
+  }
+}
