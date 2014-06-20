fcf99fcda64a1a6fc551a12ee4cb5a0e5039a383
Andrew Proshkin <zx80live@gmail.com>
2014-06-20 19:50:00 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/model/MovePatterns.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/model/MovePatterns.scala
@@ -13,15 +13,16 @@ trait MovePatterns {
    * Find preferred move patterns by current suit.
    * Special cards (e.g. "eights", "jokers" will be filtered)
    *
-   * @example {{{
+   * @example {{
    *
-   *           cards:   [ 2♦][ 3♣][ 2♣][ A♦][ 8♦][ 8♠][ 2♠][ A♣][ ☆ ]
-   *           current: [ 4♦]
+   *          cards:   [ 2♦][ 3♣][ 2♣][ A♦][ 8♦][ 8♠][ 2♠][ A♣][ ☆ ]
+   *          current: [ 4♦]
    *
-   *           preferred move patterns:
-   *                (0) [ 2♦], [ 2♣], [ 2♠]   <- highest priority (because there are maximum cards for move)
-   *                (1) [ A♦], [ A♣]
-   *          }}}
+   *          preferred move patterns:
+   *          (0) [ 2♦], [ 2♣], [ 2♠]   <- highest priority (because there are maximum cards for move)
+   *          (1) [ A♦], [ A♣]
+   *
+   *          }}
    *
    * @param current - current card
    * @param cards - player's cards
@@ -45,10 +46,11 @@ trait MovePatterns {
     }
   }
 
-  def findPreferableByRank(current: Card, cards: List[Card]): Option[List[Card]] = ???
+  def findPreferredByRank(current: Card, cards: List[Card]): Option[List[Card]] = ???
 
-  def findEight(current: Card, cards: List[Card]):Option[Card] = ???
+  def findEight(current: Card, cards: List[Card]): Option[Card] = ???
 
-  def findJoker(current: Card, cards: List[Card]):Option[Card] = ???
+  def findJoker(current: Card, cards: List[Card]): Option[Card] = ???
 
+  def findPreferrable(current: Card, cards: List[Card]): Option[List[Card]] = ???
 }
