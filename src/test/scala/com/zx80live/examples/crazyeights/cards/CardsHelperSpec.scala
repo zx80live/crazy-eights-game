a7e31028e9beea4bcfe99d090b8a271b5c59d19b
Andrew Proshkin <zx80live@gmail.com>
2014-06-22 14:50:56 +0400

    #

--- src/test/scala/com/zx80live/examples/crazyeights/cards/CardsHelperSpec.scala
+++ src/test/scala/com/zx80live/examples/crazyeights/cards/CardsHelperSpec.scala
@@ -1,5 +1,6 @@
 package com.zx80live.examples.crazyeights.cards
 
+import com.zx80live.examples.crazyeights.cards.rules.Crazy8ShuffleCondition
 import org.scalatest.{Matchers, WordSpec}
 
 /**
@@ -13,5 +14,19 @@ class CardsHelperSpec extends WordSpec with Matchers with CardsHelper {
 
   "get lazy 54-cards deck" in {
     deck54.length shouldEqual 54
+  }
+
+  "shuffle" when {
+    "without conditions" in {
+      val shuffled = shuffle(deck54)
+      shuffled.length shouldEqual deck54.length
+      shuffled foreach (deck54.contains(_))
+    }
+
+    "with crazy8 conditions" in {
+      val shuffled = shuffle(deck54, Crazy8ShuffleCondition)
+      shuffled.length shouldEqual deck54.length
+      shuffled foreach (deck54.contains(_))
+    }
   }
 }
