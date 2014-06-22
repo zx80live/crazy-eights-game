3b1af41c985fa4b4544e83f34ede13eca1cfee17
Andrew Proshkin <zx80live@gmail.com>
2014-06-22 16:39:16 +0400

    #

--- src/test/scala/com/zx80live/examples/crazyeights/cards/rules/crazy8/Crazy8WorkspaceSpec.scala
+++ src/test/scala/com/zx80live/examples/crazyeights/cards/rules/crazy8/Crazy8WorkspaceSpec.scala
@@ -1,6 +1,8 @@
 package com.zx80live.examples.crazyeights.cards.rules.crazy8
 
-import com.zx80live.examples.crazyeights.cards.CardsHelper
+import com.zx80live.examples.crazyeights.cards.Rank._
+import com.zx80live.examples.crazyeights.cards.Suit._
+import com.zx80live.examples.crazyeights.cards.{CardsHelper, Rank, Suit}
 import org.scalatest.{Matchers, WordSpec}
 
 /**
@@ -17,6 +19,8 @@ class Crazy8WorkspaceSpec extends WordSpec with Matchers with CardsHelper {
       ws.discardPile.length shouldEqual 1
       ws.currentCard shouldEqual ws.discardPile.head
       ws.stockPile.contains(ws.currentCard) shouldEqual false
+      ws.currentCard.rank should not be Eight
+      ws.currentCard.suit should not be Special
     }
   }
 }
