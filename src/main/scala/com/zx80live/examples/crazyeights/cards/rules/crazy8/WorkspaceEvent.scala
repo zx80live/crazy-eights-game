13d6b013a8cc8857a5851fac0832d1f96c02022b
Andrew Proshkin <zx80live@gmail.com>
2014-06-23 01:18:40 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/cards/rules/crazy8/WorkspaceEvent.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/cards/rules/crazy8/WorkspaceEvent.scala
@@ -1,5 +1,7 @@
 package com.zx80live.examples.crazyeights.cards.rules.crazy8
 
+import com.zx80live.examples.crazyeights.cards.Suit
+
 /**
  * @author Andrew Proshkin
  *
@@ -9,3 +11,11 @@ package com.zx80live.examples.crazyeights.cards.rules.crazy8
 case class WorkspaceEvent[T](param: T)
 
 sealed class RecreateStockpileEvent[String] extends WorkspaceEvent("stockpile is empty, recreate them from discard pile")
+
+class DiscardEvent
+
+sealed class JokerDiscardEvent extends DiscardEvent
+
+sealed class EightDiscardEvent(suit: Suit.Value) extends DiscardEvent
+
+sealed class SuccessDiscardEvent extends DiscardEvent
