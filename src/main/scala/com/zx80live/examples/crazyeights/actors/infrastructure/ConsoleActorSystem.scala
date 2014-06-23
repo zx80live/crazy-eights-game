938d972622fab59a4b9a9fb27fb1c97c05b6c3f1
Andrew Proshkin <zx80live@gmail.com>
2014-06-23 09:15:45 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/actors/infrastructure/ConsoleActorSystem.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/actors/infrastructure/ConsoleActorSystem.scala
@@ -11,48 +11,9 @@ import com.zx80live.examples.crazyeights.actors.Messages.NewGame
  */
 object ConsoleActorSystem extends App with GameHelp {
 
-  val system = ActorSystem("GameActorSystem")
+  val system = ActorSystem("ConsoleActorSystem")
   val master = system.actorOf(Props[MasterActor], name = "master")
 
   master ! NewGame(2)
-
-  //var cards: List[Card] = Nil
-
-  //  implicit private val timeout: Timeout = Duration.create(2, TimeUnit.SECONDS)
-  //
-  //  var ok = true
-  //  val defaultPrompt = "cmd:>"
-  //  var prompt = defaultPrompt
-  //
-  //  while (ok) {
-  //    scala.io.StdIn.readLine(prompt) match {
-  //      case r"new (\d+)$count" =>
-  //        master ! NewGame(value)
-  //
-  //
-  //      case "status" | "stat" | "st" =>
-  //        master ! WorkspaceStatus()
-  //
-  //      case "about" =>
-  //        printAbout()
-  //
-  //      case "help" | "h" | "?" =>
-  //        printHelp()
-  //
-  //      case "terms" | "t" =>
-  //        printTerms()
-  //
-  //      case "rules" | "r" =>
-  //        printRules()
-  //
-  //
-  //      case "exit" | "e" | "quit" | "q" | "end" =>
-  //        system.shutdown()
-  //        println("Bye!")
-  //        ok = false
-  //      case s@_ =>
-  //        println(s"unknown command, You can use 'help', 'rules'")
-  //    }
-  //  }
 
 }
