abe7d13719c5c6e4faa499f0e72a8fe8fdfcfce3
Andrew Proshkin <zx80live@gmail.com>
2014-06-23 09:42:27 +0400

    #

--- src/main/scala/com/zx80live/examples/crazyeights/actors/infrastructure/ConsoleActor.scala
+++ src/main/scala/com/zx80live/examples/crazyeights/actors/infrastructure/ConsoleActor.scala
@@ -5,12 +5,13 @@ import com.zx80live.examples.crazyeights.actors.Messages._
 import com.zx80live.examples.crazyeights.cards.Card
 import com.zx80live.examples.crazyeights.cards.dsl.ConversionUtils._
 import com.zx80live.examples.crazyeights.cards.rules.ReadonlyWorkspace
+import com.zx80live.examples.crazyeights.cards.rules.crazy8.Crazy8MovePatterns
 
 /**
  *
  * @author Andrew Proshkin
  */
-class ConsoleActor extends Actor with ActorLogging {
+class ConsoleActor extends Actor with ActorLogging with Crazy8MovePatterns {
   var cards: List[Card] = Nil
   var workspace: Option[ReadonlyWorkspace] = None
 
@@ -51,16 +52,28 @@ class ConsoleActor extends Actor with ActorLogging {
     log.info(s"\nyour cards: $cards")
     log.info("\nenter pass|p|draw|d|exit|e or comma-separated cards:>")
     scala.io.StdIn.readLine() match {
+
       case "draw" | "d" =>
         log.info("request draw card")
         sender ! Draw()
+
       case "suggest" | "sg" =>
-        log.warning("TODO suggest")
+        findPreferred(workspace.get.currentCard, cards) match {
+          case preferred if preferred.length > 0 =>
+            log.info(s"the preferred move is: $preferred")
+          case _ =>
+            log.info("can't find preferred move, use commands draw|pass")
+        }
+        enterCommand()
+
       case "pass" | "p" =>
         log.warning("pass move")
         sender ! Pass()
+
       case "exit" | "e" =>
         log.warning("TODO exit")
+        enterCommand()
+
       case xs@_ =>
         val parsedCards: Option[List[Card]] = cards"$xs"
         parsedCards match {
